d903b2d29c54eb658f7f2a6f30e620e976628ee9
Vinayak Kapoor <krrishkapoor97@gmail.com>
2023-05-07 14:12:15 -0700

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import SlotMachineScroll from "./components/slot-machine-scroll/SlotMachineScroll";
-import "./base.css";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-import { useRef } from "react";
 
 function App() {
-    const containerRef = useRef(null);
 
     return (
-        <LocomotiveScrollProvider
-            options={{
-                smooth: true,
-            }}
-            watch={[]}
-            containerRef={containerRef}
-        >
-            <div data-scroll-container ref={containerRef}>
-                <div
-                    style={{ height: "100vh", backgroundColor: "beige" }}
-                    data-scroll-section
-                />
-                <SlotMachineScroll/>
-            </div>
-        </LocomotiveScrollProvider>
+        <div>
+            <div style={{ height: "100vh", backgroundColor: "black" }} />
+            <SlotMachineScroll />
+        </div>
     );
 }
 
