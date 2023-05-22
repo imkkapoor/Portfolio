e34cf240a9080b468f449101ffdd31f30a62e88e
Vinayak Kapoor <krrishkapoor97@gmail.com>
2023-05-22 14:35:18 -0700

    updates

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./Intro.css";
-import mac from "../../assets/m2_laptop_hw__c84jsepabw2u_large_2x.jpg";
-import screen from "../../assets/screen_processing__f36dk3ge7wey_large_2x.jpg";
-
 
 export default function Intro() {
     return (
         <div className="intro-container">
             <div className="intro-wrapper">
-                <img src={mac} alt="mac" />
-                <img src = {screen}/>
+                
 
-                <div className="content" >
+                <div className="content">
                     {/* <h1>Hi,</h1>
                     <h1>I am Vinayak</h1>
                     <h3>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import "./Projects.css";
+import pj1 from "../../assets/pj1.png";
+
 export default function Projects() {
     return (
-        <div className="projects-constainer">
+        <div className="projects-container">
             <div className="projects-wrapper">
-                <div className="first-project">
-                    <h1>SOURCERY</h1>
+                <div className="project">
+                    <div className="content">
+                        <div className="header">SOURCERY</div>
+                        Developed a dynamic website using the MERN stack,
+                        implementing a REST API with all essential CRUD
+                        operations and user authentication through JWT for
+                        improved security<br></br> • Built a modern, responsive,
+                        and visually appealing user interface that allows
+                        customers to browse products, add them to their cart,
+                        and checkout with ease <br></br>• Crafted user
+                        login/register pathways, integrated stripe payment
+                        gateway, added redux persist for state management and
+                        utilized CryptoJs encryption for increased security
+                        Developed a Full-Stack Ecommerce website on MERN stack.
+                    </div>
+                    <img src={pj1} alt="mac" />
                 </div>
-                <div className="second-project">
-                    <h1>CONCORDANCE</h1>
+
+                <div className="project">
+                    <div className="content"></div>
                 </div>
-                <div className="third-project">
-                    <h1>COVID-19 MANAGEMENT SYSTEM</h1>
+                <div className="project">
+                    <div className="content"></div>
+                </div>
+                <div className="project">
+                    <div className="content"></div>
                 </div>
             </div>
         </div>
