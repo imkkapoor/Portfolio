32b1f14eb3691edca48f531bb990414ac67eb34c
Vinayak Kapoor <krrishkapoor97@gmail.com>
2023-05-25 15:40:27 -0700

    major updates - projects up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Intro from "./components/intro/Intro";
-import NavBar from "./components/nav-bar/NavBar";
 import Projects from "./components/projects/Projects";
 import SlotMachineScroll from "./components/slot-machine-scroll/SlotMachineScroll";
 
 function App() {
     return (
         <div>
-            <NavBar />
             <Intro />
             <Projects />
             {/* <div style={{ height: "100vh", backgroundColor: "#161616" }} /> */}
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Projects.css";
-import pj1 from "../../assets/pj1.png";
-import cmd1 from "../../assets/cmd2.png";
-import cmdResult from "../../assets/cmd-result.png";
+import pj1 from "../../assets/pj1-re.png";
+import cmd1 from "../../assets/cmd2-2.png";
+import cmdResult from "../../assets/cmd-result-2.png";
 import graph from "../../assets/graph.png";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkIcon from "@mui/icons-material/Link";
@@ -12,86 +12,95 @@ export default function Projects() {
         <div className="projects-container">
             <div className="projects-wrapper">
                 <div className="project">
-                    <div className="content">
-                        <div className="header">Sourcery</div>
-                        <p>
-                            I developed a dynamic MERN stack website with a REST
-                            API, user authentication, CRUD operations, and a
-                            responsive UI for ecommerce functionality including
-                            product browsing, cart management, and secure
-                            checkout with Stripe integration.
-                        </p>
-                        <ul className="languages">
-                            <li>React</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>Node</li>
-                            <li>MongoDB</li>
-                            <li>Express</li>
-                        </ul>
-                        <div className="links">
-                            <a href="https://github.com/imkkapoor/Sourcery">
-                                <GitHubIcon />
-                            </a>
-                            <a href="https://sourcery604.netlify.app/">
-                                <LinkIcon />
-                            </a>
+                    <div className="project-index">01</div>
+                    <div className="content-wrapper">
+                        <div className="content">
+                            <div className="header">Sourcery</div>
+                            <p>
+                                Developed a dynamic MERN stack website with a
+                                REST API, user authentication, CRUD operations,
+                                and a responsive UI for ecommerce functionality
+                                including product browsing, cart management, and
+                                secure checkout with Stripe integration.
+                            </p>
+                            <ul className="languages">
+                                <li>React</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                                <li>Node</li>
+                                <li>MongoDB</li>
+                                <li>Express</li>
+                            </ul>
+                            <div className="links">
+                                <a href="https://github.com/imkkapoor/Sourcery">
+                                    <GitHubIcon />
+                                </a>
+                                <a href="https://sourcery604.netlify.app/">
+                                    <LinkIcon />
+                                </a>
+                            </div>
                         </div>
+                        <img src={pj1} alt="mac" />
                     </div>
-                    <img src={pj1} alt="mac" />
                 </div>
 
                 <div className="project">
-                    <img src={cmd1} alt="cmd-in" />
+                    <div className="project-index">02</div>
+                    <div className="content-wrapper">
+                        <img src={cmd1} alt="cmd-in" />
 
-                    <div className="content">
-                        <div className="header">Concordance</div>
-                        <p>
-                            The idea is that concordance permits you to read or
-                            see a word that has been indexed in its original
-                            context, as this can often aid the reader to decide
-                            whether or not the reference is indeed what they
-                            want or need.
-                        </p>
-                        <ul className="languages">
-                            <li>C</li>
-                            <li>Python</li>
-                        </ul>
-                        <div className="links">
-                            <a href="https://github.com/imkkapoor/Concordance">
-                                <GitHubIcon />
-                            </a>
+                        <div className="content">
+                            <div className="header">Concordance</div>
+                            <p>
+                                Concordance permits you to read
+                                or see a word that has been indexed in its
+                                original context, as this can often aid the
+                                reader to decide whether or not the reference is
+                                indeed what they want or need.
+                            </p>
+                            <ul className="languages">
+                                <li>C</li>
+                                <li>Python</li>
+                            </ul>
+                            <div className="links">
+                                <a href="https://github.com/imkkapoor/Concordance">
+                                    <GitHubIcon />
+                                </a>
+                            </div>
                         </div>
+                        <img src={cmdResult} alt="cmd-result" />
                     </div>
-                    <img src={cmdResult} alt="cmd-result" />
                 </div>
                 <div className="project">
-                    <div className="content">
-                        <div className="header">Covid Management Sys</div>
-                        <p>
-                            Covid management system helps us to
-                            store,update,view and represent data of covid
-                            patients and the medical staff involved with the
-                            help of a python code linked with MySQL.
-                        </p>
-                        <ul className="languages">
-                            <li>Python</li>
-                            <li>MySQL</li>
-                            <li>Pandas</li>
-                            <li>Matplotlib</li>
-                        </ul>
-                        <div className="links">
-                            {" "}
-                            <a href="https://github.com/imkkapoor/Covid-Management-System">
-                                <GitHubIcon />
-                            </a>
+                    <div className="project-index">03</div>
+                    <div className="content-wrapper">
+                        <div className="content">
+                            <div className="header">Covid Management Sys</div>
+                            <p>
+                                Covid management system helps us to
+                                store,update,view and represent data of covid
+                                patients and the medical staff involved with the
+                                help of a python code linked with MySQL.
+                            </p>
+                            <ul className="languages">
+                                <li>Python</li>
+                                <li>MySQL</li>
+                                <li>Pandas</li>
+                                <li>Matplotlib</li>
+                            </ul>
+                            <div className="links">
+                                {" "}
+                                <a href="https://github.com/imkkapoor/Covid-Management-System">
+                                    <GitHubIcon />
+                                </a>
+                            </div>
                         </div>
+                        <img
+                            src={graph}
+                            alt="graph"
+                            style={{ borderRadius: "15px" }}
+                        />
                     </div>
-                    <img
-                        src={graph}
-                        alt="graph"
-                        style={{ borderRadius: "15px" }}
-                    />
                 </div>
             </div>
         </div>
