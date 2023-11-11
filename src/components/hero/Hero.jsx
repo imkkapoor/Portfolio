01e5a33e44fd69a029ba282a8aacee67179ed2dd
Vinayak Kapoor <vkapoor@latitudegeo.com>
2023-11-10 20:55:08 -0800

    hero updated plus major css/structure changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import "./Hero.css";
+import './Hero.css';
 
 export default function Hero() {
   return (
     <div className="hero-container">
-      <div className="hero-wrapper"></div>
+      <div className="hero-wrapper">
+        <div className="hero-caption">
+          Brewing <span>code potions</span> that would even impress Snape
+        </div>
+      </div>
     </div>
   );
 }
