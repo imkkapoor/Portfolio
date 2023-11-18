d480ab5b7ec2427a2017f027e48891c4daeed1a5
Vinayak Kapoor <72612085+imkkapoor@users.noreply.github.com>
2023-11-18 00:08:09 -0800

    navigation updated

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Hero.css';
-// import memeoji from '../../assets/memeoji.png';
 
 export default function Hero() {
   return (
+    <>
+    <div id='hero'/>
     <div className="hero-container">
       <div className="hero-wrapper">
         <div className="hero-caption">
           Brewing <span>code potions</span> that would even impress Snape
         </div>
       </div>
-      {/* <img src={memeoji} alt="face" /> */}
     </div>
+    </>
   );
 }
