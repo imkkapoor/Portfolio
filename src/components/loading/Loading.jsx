e5d55e2353e6d8a49c0dd86f2e1713ff7c28d1a1
Vinayak Kapoor <vkapoor@latitudegeo.com>
2023-11-20 23:36:15 -0800

    cleanup and framer motion added

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './Loading.css';
 
+
 export default function Loading() {
+  
   return (
-    <div className="loading-container">
-      <div className="loading-wrapper">
-        <div class="spinner">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+      <div className="loading-container">
+        <div className="loading-wrapper">
+          <div className="spinner">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
